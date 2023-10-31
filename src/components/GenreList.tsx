ca9f5bb17159393106f56a9243cd61db21f1c3a5
austinevick <you@example.com>
2023-10-31 11:08:00 +0100

    modified:   src/components/GenreList.tsx

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react"
+import { Button, HStack, Heading, Image, List, ListItem } from "@chakra-ui/react"
 import { Genre, useGenres } from "../hooks/useGenres"
 import { GameListSkeleton } from "./GameCardSkeleton"
 
@@ -11,16 +11,19 @@ export const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     const { genres, isLoading } = useGenres()
     const skeletons = [1, 2, 3, 4, 5, 6]
     return (
-        <List>
-            {isLoading && skeletons.map((e) => (<GameListSkeleton key={e} />))}
+        <>
+            <Heading marginY='20px'>Genres</Heading>
+            <List>
+                {isLoading && skeletons.map((e) => (<GameListSkeleton key={e} />))}
 
-            {genres.map((genre) => (<ListItem key={genre.id} paddingY={'5px'}>
-                <HStack>
-                    <Image borderRadius={8} boxSize={'52px'} src={genre.image_background} />
-                    <Button fontWeight={genre.id === selectedGenre?.id ? 'black' : 'normal'} onClick={() => onSelectGenre(genre)
-                    } variant={'link'} fontSize={'lg'}>{genre.name}</Button>
-                </HStack>
-            </ListItem>))}
-        </List>
+                {genres.map((genre) => (<ListItem key={genre.id} paddingY={'5px'}>
+                    <HStack>
+                        <Image objectFit={'cover'} borderRadius={8} boxSize={'52px'} src={genre.image_background} />
+                        <Button whiteSpace={'normal'} textAlign={'left'} fontWeight={genre.id === selectedGenre?.id ? 'black' : 'normal'} onClick={() => onSelectGenre(genre)
+                        } variant={'link'} fontSize={'lg'}>{genre.name}</Button>
+                    </HStack>
+                </ListItem>))}
+            </List>
+        </>
     )
 }
