c23abeb55fe1319de8e892157c53e57044b88a79
austinevick <you@example.com>
2023-11-03 03:38:58 +0100

    modified:   package-lock.json 	modified:   package.json 	new file:   src/assets/bulls-eye.webp 	new file:   src/assets/meh.webp 	new file:   src/assets/no-image-placeholder-6f3882e0.webp 	new file:   src/assets/thumbs-up.webp 	new file:   src/components/Emoji.tsx 	modified:   src/components/GameCard.tsx 	modified:   src/hooks/useGames.ts

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import bullsEye from '../assets/bulls-eye.webp'
+import thumbsUp from '../assets/thumbs-up.webp'
+import meh from '../assets/meh.webp'
+import { Image, ImageProps } from '@chakra-ui/react'
+
+
+
+export const Emoji = ({ rating }: { rating: number }) => {
+    const emojiMap: { [key: number]: ImageProps } = {
+        3: { src: meh, alt: 'meh' },
+        4: { src: thumbsUp, alt: 'recommended' },
+        5: { src: bullsEye, alt: 'exceptional' },
+    }
+
+
+    if (rating < 3) return null
+    return (
+        <Image {...emojiMap[rating]} boxSize='25px' marginTop={2} />
+    )
+}
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
 import { PlatformIconList } from "./PlatformIconList"
 import { CriticScore } from "./CriticScore"
+import { Emoji } from "./Emoji"
 
 interface Props {
     game: Game
@@ -13,7 +14,7 @@ export const GameCard = ({ game }: Props) => {
         <Card borderRadius={10} overflow={'hidden'}>
             <Image src={game.background_image} />
             <CardBody height={'20px'}>
-                <Heading fontSize={'16px'}>{game.name}</Heading>
+                <Heading fontSize={'16px'}>{game.name}<Emoji rating={game.rating_top} /> </Heading>
                 <HStack justifyContent={'space-between'}>
 
                     <PlatformIconList platforms={game.parent_platforms.map((e) => e.platform)} />
